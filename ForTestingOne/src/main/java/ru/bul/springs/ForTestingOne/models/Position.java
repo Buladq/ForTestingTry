29f6ad9e2c077e5d71bdb021cf9a4922ca12ea48
Buladq <chumakov.bulat@yandex.ru>
2023-05-25 00:10:35 +0300

    create model

---
+++ ForTestingOne/src/main/java/ru/bul/springs/ForTestingOne/models/Position.java
@@ -0,0 +1,53 @@
+package ru.bul.springs.ForTestingOne.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "position")
+public class Position {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+
+    public Position(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Position() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
