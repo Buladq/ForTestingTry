77d9ecc2e303bbd33ab461980f609c4af18a2d0c
Buladq <chumakov.bulat@yandex.ru>
2023-05-25 00:02:11 +0300

    build project for start

---
+++ ForTestingOne/src/main/java/ru/bul/springs/ForTestingOne/ForTestingOneApplication.java
@@ -0,0 +1,13 @@
+package ru.bul.springs.ForTestingOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForTestingOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForTestingOneApplication.class, args);
+	}
+
+}
---
+++ ForTestingOne/src/test/java/ru/bul/springs/ForTestingOne/ForTestingOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bul.springs.ForTestingOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForTestingOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
